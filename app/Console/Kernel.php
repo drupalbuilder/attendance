82570a536036e66542734a2e6dacc40c12ec08bf
drupalbuilder-shubham <133983331+drupalbuilder-shubham@users.noreply.github.com>
2024-01-03 13:10:17 +0530

    Revert "add"
    This reverts commit c0c15edc517e884ab0356298a86d47848869915c.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,58 +8,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        Commands\whatsapp::class,
-        Commands\whatsappCallingVerify::class,
-        Commands\whatsappCallingVerifyPerHour::class,
-        Commands\whatsappCallingVerifyPerDay::class,
-        Commands\WhatsappReminder::class,
-        Commands\CustomerCall::class,
-        Commands\FeedbackSubmission::class,
-        Commands\ReminderFeedbackSubmission::class,
-        commands\FollowUpFeedback::class,
-        commands\WhatsappReminderAfterSevenDays::class,
-        commands\SegmentScheduler::class,
-        commands\LeadUpdate::class,
-    ];
-
-    /**
      * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('whatsapp:send')->everyMinute();
-       // $schedule->command('whatsapp:verify')->everyMinute();
-       // $schedule->command('whatsapp:verifyPerHour')->hourly();
-       // $schedule->command('whatsapp:verifyPerDay')->daily();
-        // $schedule->command('feedback:submission')->hourly();
-        // $schedule->command('reminderfeedback:submission')->hourly();
-        // $schedule->command('followup:feedback')->hourly();
-        // $schedule->command('whatsapp:reminder-msg')->hourly();
-        // $schedule->command('reminder:seven-days')->hourly();
-        // $schedule->command('customer:call')->hourly();
-        
-       // $schedule->command('crm:update')->everyMinute();
-        // $schedule->command('notification:send')->dailyAt('9:00');
-        $schedule->command('send:EmiPlanEmail')->dailyAt('12:00');
-        $schedule->command('send:EmiPlanEmail')->dailyAt('15:00');
-       // $schedule->command('segment:scheduler')->everyMinute();
-       // $schedule->command('lead:update')->hourly();
+        // $schedule->command('inspire')->hourly();
     }
 
     /**
      * Register the commands for the application.
-     *
-     * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
